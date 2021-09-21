1555fb8be43c381374fe8b590ed167b62d9aec49
kevin <dqtkien@gmail.com>
2021-09-21 14:50:06 +0700

    Add hello word front end

--- backend/knex.js
+++ backend/knex.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
   connection: {
     host: '127.0.0.1',
     port: 3306,
-    user: 'admin',
+    user: 'root',
     password: 'kienbgf',
     database: 'testdb',
   },
